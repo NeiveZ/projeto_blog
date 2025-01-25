152e648da19218fd853ac6685427ca9cb5bde416
NeiveZ <lucas.ntnt@gmail.com>
2025-01-25 14:38:39 -0400

    Fix/FOTA

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,13 +3,6 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." 
-  sleep 3
-done
-
-echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
-
 wait_psql.sh
 collectstatic.sh
 makemigrations.sh
